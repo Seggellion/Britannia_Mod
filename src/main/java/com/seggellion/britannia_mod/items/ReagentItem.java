a5584cadea78b34b6c07f65d19bd9545d144cad0
HybridAvenger69 <hybridavenger69@yahoo.com>
2024-09-30 10:58:57 -0600

    Backport fix

--- src/main/java/com/seggellion/britannia_mod/items/ReagentItem.java
+++
@@ -1,9 +0,0 @@
-package com.seggellion.britannia_mod.items;
-
-import net.minecraft.world.item.Item;
-
-public class ReagentItem extends Item {
-    public ReagentItem() {
-        super(new Item.Properties());  // The Creative Tab will be handled in your mod's event setup
-    }
-}
