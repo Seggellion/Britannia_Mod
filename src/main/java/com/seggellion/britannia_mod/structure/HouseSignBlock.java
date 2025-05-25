c9bf7e948ddb3681580a3b8dc1ae7f9f6a63507a
muriplz <46825658+muriplz@users.noreply.github.com>
2025-05-25 19:43:11 +0200

    final fixes

--- src/main/java/com/seggellion/britannia_mod/structure/HouseSignBlock.java
+++ src/main/java/com/seggellion/britannia_mod/structure/HouseSignBlock.java
@@ -7,6 +7,7 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.EntityBlock;
@@ -23,6 +24,9 @@ import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.block.state.properties.EnumProperty;
 import net.minecraft.util.StringRepresentable;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.Shapes;
+import net.minecraft.world.phys.shapes.VoxelShape;
 import org.slf4j.Logger;
 
 public class HouseSignBlock extends Block implements EntityBlock {
@@ -32,6 +36,14 @@ public class HouseSignBlock extends Block implements EntityBlock {
 
     public static final EnumProperty<SignType> SIGN_TYPE = EnumProperty.create("sign_type", SignType.class);
     public static final EnumProperty<HolderType> HOLDER_TYPE = EnumProperty.create("holder_type", HolderType.class);
+
+    @Override
+    public VoxelShape getShape(BlockState state, BlockGetter level, BlockPos pos, CollisionContext context) {
+        return Shapes.or(
+                Block.box(0, 0, 0, 16, 16, 16),
+                Block.box(16, 0, 0, 32, 16, 16)
+        );
+    }
 
     public enum HolderType implements StringRepresentable {
         WOOD("wood"),
